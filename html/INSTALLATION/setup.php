988ab46a693c5d458a7cfa3ff342b34ad5e2ba0f
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-20 15:20:57 -0400

    Update setup.php

--- html/INSTALLATION/setup.php
+++ html/INSTALLATION/setup.php
@@ -5,11 +5,11 @@ Not for distribution other than by Raven Web Services, LLC
 Copyright 2005-2013
 **/
 if (!defined('INCLUDE_PATH')) define('INCLUDE_PATH', '../');
-@require_once INCLUDE_PATH . 'includes/mimetype.php';
+require_once INCLUDE_PATH . 'includes/mimetype.php';
 $nukeConfigFile = INCLUDE_PATH . 'config.php';;
 if (!file_exists($nukeConfigFile)) {rnInstallErr(1); die();}
 require_once INCLUDE_PATH . 'config.php';
-@require_once 'ravenstallerConfigFile.php';
+require_once 'ravenstallerConfigFile.php';
 $pattern = '/([^[]*)([\]])/i';
 $rnADMIN_ALLOWED_USERID_CHARACTERS = (defined('_rnADMIN_ALLOWED_USERID_CHARACTERS') && _rnALLOWED_USERID_CHARACTERS=='*') ? _rnADMIN_ALLOWED_USERID_CHARACTERS : _rnALLOWED_USERID_CHARACTERS;
 preg_match($pattern, $rnADMIN_ALLOWED_USERID_CHARACTERS, $matches);
@@ -129,7 +129,7 @@ $dbCheck = array();
 // if (!file_exists($nukeConfigFile)) {rnInstallErr(1); die();}
 echo '<span class="msg">config.php file found!</span><br />';
 require_once INCLUDE_PATH . 'config.php';
-$conn = @mysqli_connect($dbhost,$dbuname,$dbpass);
+$conn = mysqli_connect($dbhost,$dbuname,$dbpass);
 if (!$conn)  {rnInstallErr(2); die();}
 echo '<span class="msg">Successfully connected to '.$dbhost.' as user '.$dbuname.' and assigned password!</span><br />';
 $db = mysqli_select_db($conn, $dbname);
@@ -138,7 +138,7 @@ echo '<span class="msg">Database '.$dbname.' found!</span><br />';
 ?>
 <form name="mysql" method="post" action="<?php $_SERVER['PHP_SELF'];?>">
 	<p class="c3">
-		<input type="button"  name="btnCheckMysqlUserPermissions" class="button2" id="btnCheckMysqlUserPermissions" readonly="readonly" size="75" onfocus="blur()" onclick="browserWindowSize(1.07,.7);dhtmlmodal.open('mysqlCheck', 'iframe', 'serverMysqlUserPermissionsCheck.php', 'Ravenstaller™ MySQL User Permissions Check', 'width='+bwW+',height='+bwH+',center=1,resize=1,scrolling=1');" value="Run Ravenstaller™ MySQL User Permissions Check" />
+		<input type="button"  name="btnCheckMysqlUserPermissions" class="button2" id="btnCheckMysqlUserPermissions" readonly size="75" onFocus="blur()" onClick="browserWindowSize(1.07,.7);dhtmlmodal.open('mysqlCheck', 'iframe', 'serverMysqlUserPermissionsCheck.php', 'Ravenstaller™ MySQL User Permissions Check', 'width='+bwW+',height='+bwH+',center=1,resize=1,scrolling=1');" value="Run Ravenstaller™ MySQL User Permissions Check" />
 	</p>
 </form>
 <?php
@@ -165,11 +165,11 @@ $_POST['config_nukeurl'] = trim($_POST['config_nukeurl'],'\\');
 	<table width="100%">
 		<tr>
 			<td>
-				<form onsubmit="return validateInput(configform)" name="configform" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+				<form onSubmit="return validateInput(configform)" name="configform" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 					<p class="sectiontitle">Configuration Settings For RavenNuke&trade; Core Functionality</p>
 					<table>
 						<tr>
-							<td>Site Name:</td><td><input class="inputbox" onblur="configform.bbconfig_sitename.value=this.value" size="60" name="config_sitename" value="<?php echo $_POST['config_sitename']=isset($_POST['config_sitename'])?stripslashes($_POST['config_sitename']):'';?>" /></td>
+							<td>Site Name:</td><td><input class="inputbox" onBlur="configform.bbconfig_sitename.value=this.value" size="60" name="config_sitename" value="<?php echo $_POST['config_sitename']=isset($_POST['config_sitename'])?stripslashes($_POST['config_sitename']):'';?>" /></td>
 						</tr>
 						<tr>
 							<td>Site URL (With http://):</td><td><input class="inputbox" size="60" name="config_nukeurl" value="<?php echo $_POST['config_nukeurl']; ?>" />
@@ -182,10 +182,10 @@ $_POST['config_nukeurl'] = trim($_POST['config_nukeurl'],'\\');
 							<td>Site Start Date (Freeform):</td><td><input class="inputbox" size="60" name="config_startdate" value="<?php echo $_POST['config_startdate'] = (empty($_POST['config_startdate'])?date('F d, Y'):$_POST['config_startdate']);?>" /></td>
 						</tr>
 						<tr>
-							<td>Administrator Email:</td><td><input onblur="configform.bbconfig_board_email.value=this.value;configform.authors_email.value=this.value;configform.nsnst_config_admin_contact.value=this.value;configform.users_user_email.value=this.value" class="inputbox" size="60" name="config_adminmail" value="<?php echo $_POST['config_adminmail']=isset($_POST['config_adminmail'])?$_POST['config_adminmail']:'';?>" /></td>
+							<td>Administrator Email:</td><td><input onBlur="configform.bbconfig_board_email.value=this.value;configform.authors_email.value=this.value;configform.nsnst_config_admin_contact.value=this.value;configform.users_user_email.value=this.value" class="inputbox" size="60" name="config_adminmail" value="<?php echo $_POST['config_adminmail']=isset($_POST['config_adminmail'])?$_POST['config_adminmail']:'';?>" /></td>
 						</tr>
 						<tr>
-							<td>*GOD* Administrator UserName:</td><td><input onblur="configform.nsnst_admins_login.value=this.value;configform.users_username.value=this.value;configform.nsnst_admins_aid.value=this.value" class="inputbox" size="60" name="authors_aid" value="<?php echo $_POST['authors_aid']=isset($_POST['authors_aid'])?stripslashes($_POST['authors_aid']):'';?>" />&nbsp; <span class="c2">Allowed Characters: <?php echo $rnADMIN_ALLOWED_USERID_CHARACTERS; ?></span></td>
+							<td>*GOD* Administrator UserName:</td><td><input onBlur="configform.nsnst_admins_login.value=this.value;configform.users_username.value=this.value;configform.nsnst_admins_aid.value=this.value" class="inputbox" size="60" name="authors_aid" value="<?php echo $_POST['authors_aid']=isset($_POST['authors_aid'])?stripslashes($_POST['authors_aid']):'';?>" />&nbsp; <span class="c2">Allowed Characters: <?php echo $rnADMIN_ALLOWED_USERID_CHARACTERS; ?></span></td>
 						</tr>
 						<tr>
 							<td>*GOD* Administrator Password:</td><td><input class="inputbox" size="60" name="authors_pwd" value="<?php echo $_POST['authors_pwd']=isset($_POST['authors_pwd'])?$_POST['authors_pwd']:'';?>" />&nbsp; <span class="c2">Must be between <?php echo (_rnMINIMUM_PASSWORD_LENGTH)>0?_rnMINIMUM_PASSWORD_LENGTH:_rnADMIN_MINIMUM_PASSWORD_LENGTH;?> and <?php echo (_rnMAXIMUM_PASSWORD_LENGTH)>0?_rnMAXIMUM_PASSWORD_LENGTH:_rnADMIN_MAXIMUM_PASSWORD_LENGTH;?> bytes</span></td>
@@ -257,53 +257,53 @@ if (isset($_POST['updateconfig']) AND $_POST['updateconfig']=='Save Settings') {
 
 	/* Posting data to nuke_config table */
 	$sql = "UPDATE ".$prefix."_config set sitename='".mysqli_real_escape_string($conn, stripslashes($config_sitename))."', nukeurl='$config_nukeurl', slogan='".mysqli_real_escape_string($conn, stripslashes($config_slogan))."', startdate='$config_startdate', adminmail='$config_adminmail'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_config table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Posting data to nuke_authors table */
 	$sql = 'TRUNCATE TABLE '.$prefix.'_authors';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to truncate '.$prefix.'_authors table. MySQL reported: '.mysqli_error($conn));
 	$sql = "INSERT INTO ".$prefix."_authors (name,aid,pwd,email,radminsuper) values('God','$authors_aid','".md5($authors_pwd)."','$authors_email','$authors_radminsuper')";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$prefix.'_authors table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Posting data to nuke_bbconfig table */
 	$sql = "UPDATE ".$prefix."_bbconfig set config_value='".mysqli_real_escape_string($conn, stripslashes($bbconfig_server_name))."' WHERE config_name='server_name'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_bbconfig table (server_name). MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = "UPDATE ".$prefix."_bbconfig set config_value='".mysqli_real_escape_string($conn, stripslashes($bbconfig_cookie_domain))."' WHERE config_name='cookie_domain'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_bbconfig table (cookie_domain). MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	if (trim($bbconfig_cookie_path, '/') == '') $bbconfig_cookie_path = '/';
 	else $bbconfig_cookie_path = '/'.trim($bbconfig_cookie_path, '/').'/';
 	$sql = "UPDATE ".$prefix."_bbconfig set config_value='".mysqli_real_escape_string($conn, stripslashes($bbconfig_cookie_path))."' WHERE config_name='cookie_path'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_bbconfig table (cookie_path). MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = "UPDATE ".$prefix."_bbconfig set config_value='".mysqli_real_escape_string($conn, stripslashes($bbconfig_sitename))."' WHERE config_name='sitename'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_bbconfig table (sitename). MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = "UPDATE ".$prefix."_bbconfig set config_value='".mysqli_real_escape_string($conn, stripslashes($bbconfig_board_email))."' WHERE config_name='board_email'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_bbconfig table (board_email). MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Posting data to nuke_nsnst_config table */
 	$sql = "UPDATE ".$prefix."_nsnst_config set config_value='".mysqli_real_escape_string($conn, stripslashes($nsnst_config_admin_contact))."' WHERE config_name='admin_contact'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_nsnst_config table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = "UPDATE ".$prefix."_nsnst_config set config_value='1' WHERE config_name='track_active'";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update '.$prefix.'_nsnst_config table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = 'UPDATE `' . $prefix . '_nsnst_config` SET `config_value`="' . _nsIP2C . '" WHERE `config_name`="ip2c_version"';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Update ' . $prefix . '_nsnst_config table.  MySQL reported: ' . mysqli_error($conn) . '<br />' . $sql);
 
 	/* Posting data to nuke_nsnst_admins table */
 	$sql = 'TRUNCATE TABLE '.$prefix.'_nsnst_admins';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to truncate '.$prefix.'_nsnst_admins table. MySQL reported: '.mysqli_error($conn));
 	$sql = "INSERT INTO ".$prefix."_nsnst_admins (login, password, protected, aid, password_md5, password_crypt) values('$nsnst_admins_login','$nsnst_admins_password','$nsnst_admins_protected','$nsnst_admins_aid','".md5($nsnst_admins_password)."', '".crypt($nsnst_admins_password, '')."')";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$prefix.'_nsnst_admins table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Protecting admins IP */
@@ -311,60 +311,60 @@ if (isset($_POST['updateconfig']) AND $_POST['updateconfig']=='Save Settings') {
 	$long_ip = sprintf("%u", ip2long($ip));
 	$time = time();
 	$sql = 'TRUNCATE TABLE '.$prefix.'_nsnst_excluded_ranges';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to truncate '.$prefix.'_nsnst_excluded_ranges table. MySQL reported: '.mysqli_error($conn));
 	$sql = "INSERT INTO `".$prefix."_nsnst_excluded_ranges` (`ip_lo`, `ip_hi`, `date`, `notes`, `c2c`) VALUES ('".$long_ip."', '".$long_ip."', '".$time."', '".$authors_aid."\'s personal ip', '00')";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$prefix.'_nsnst_excluded_ranges table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 	$sql = 'TRUNCATE TABLE '.$prefix.'_nsnst_protected_ranges';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to truncate '.$prefix.'_nsnst_protected_ranges table. MySQL reported: '.mysqli_error($conn));
 	$sql = "INSERT INTO `".$prefix."_nsnst_protected_ranges` (`ip_lo`, `ip_hi`, `date`, `notes`, `c2c`) VALUES ('".$long_ip."', '".$long_ip."', '".$time."', '".$authors_aid."\'s personal ip', '00')";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$prefix.'_nsnst_protected_ranges table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Posting data to nuke_users table */
 	$sql = 'TRUNCATE TABLE '.$user_prefix.'_users';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to truncate '.$user_prefix.'_users table. MySQL reported: '.mysqli_error($conn));
 	$sql = "INSERT INTO ".$user_prefix."_users VALUES (1, '', 'Anonymous', '', '', '', 'blank.gif', 'Oct 10, 2008', '', '', '', '', '', 0, 0, '', '', '', '', 10, '', 0, 0, 0, '', 0, '', '', 4096, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 10, NULL, 'english', 'D M d, Y g:i a', 0, 0, 0, NULL, 1, 1, 1, 1, 1, 1, 1, 1, 0, 3, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0)";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$user_prefix.'_users table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Raven added user_level=2 per Mantis 0001175 */
 	$sql = "INSERT INTO ".$user_prefix."_users (user_id, user_avatar, user_regdate, username, user_email, user_password, name, user_level) values(2, '$users_user_avatar', '$users_user_regdate', '".mysqli_real_escape_string($conn, stripslashes($users_username))."','$users_user_email','".md5($users_user_password)."', '".mysqli_real_escape_string($conn, stripslashes($users_username))."', 2)";
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to Insert into '.$user_prefix.'_users table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/*Added by montego for NSN Group fix 0000291 */
 	//$sql = "UPDATE ".$prefix."_nsngr_users SET uname = '".mysqli_real_escape_string($conn, stripslashes($users_username))."'";
-	// $rc = @mysqli_query($conn, $sql);
+	// $rc = mysqli_query($conn, $sql);
 	// if (!$rc) die('Unable to update '.$prefix.'_nsngr_users table. MySQL reported: '.mysqli_error($conn)."<br />$sql");
 
 	/* Updating the image source on block 'Information' to be absolute so will work with the FCKEditor */
 	$sql = 'SELECT `content` FROM `'.$prefix.'_blocks` WHERE `title` = \'Information\'';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if ($rc) {
 		list($content) = mysqli_fetch_row($rc);
 		if ($content != '') {
 			/** The next line is Fix per Mantis Issue 0001298 and is suggested by Palbin **/
 			$content = preg_replace('#(' . preg_quote($config_nukeurl) . '/)?images/#i', $config_nukeurl . '/images/', $content);
 			$sql = 'UPDATE `'.$prefix.'_blocks` SET `content` = \''.$content.'\' WHERE `title` = \'Information\'';
-			$rc = @mysqli_query($conn, $sql);
+			$rc = mysqli_query($conn, $sql);
 			if (!$rc) die('Unable to update '.$prefix.'_blocks table for absolute image src URL replacement. MySQL reported: '.mysqli_error($conn).'<br />'.$sql);
 		}
 	}
 
 	/* Updating the image source on message 'Welcome Message' to be absolute so will work with the FCKEditor */
 	$sql = 'SELECT `content` FROM `'.$prefix.'_message` WHERE `mid` = 1';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if ($rc) {
 		list($content) = mysqli_fetch_row($rc);
 		if ($content != '') {
 			/** The next line is Fix per Mantis Issue 0001298 and is suggested by Palbin **/
 			$content = preg_replace('#(' . preg_quote($config_nukeurl) . '/)?images/#i', $config_nukeurl . '/images/', $content);
 			$sql = 'UPDATE `'.$prefix.'_message` SET `content` = \''.$content.'\' WHERE `mid` = 1';
-			$rc = @mysqli_query($conn, $sql);
+			$rc = mysqli_query($conn, $sql);
 			if (!$rc) die('Unable to update '.$prefix.'_message table for absolute image src URL replacement. MySQL reported: '.mysqli_error($conn).'<br />'.$sql);
 		}
 	}
@@ -399,15 +399,15 @@ if (isset($_POST['updateconfig']) AND $_POST['updateconfig']=='Save Settings') {
 
 	if ($values_add) {
 		$sql = 'TRUNCATE TABLE ' . $prefix . '_themes';
-		$rc = @mysqli_query($conn, $sql);
+		$rc = mysqli_query($conn, $sql);
 		if (!$rc) die('Unable to truncate ' . $prefix . '_themes table. MySQL reported: ' . mysqli_error($conn));
 		$sql = 'INSERT INTO `' . $prefix . '_themes` (`theme`, `themename`, `active`, `default`, `guest`, `moveableblocks`, `collapsibleblocks`, `compatible`) VALUES ' . $values_add;
-		$rc = @mysqli_query($conn, $sql);
+		$rc = mysqli_query($conn, $sql);
 		if (!$rc) die('Unable to update ' . $prefix . '_themes table to populate it with themes. MySQL reported: ' . mysqli_error($conn) . '<br />' . $sql);
 	}
 
 	$sql = 'UPDATE `' . $prefix . '_themes` SET `default`="1", `guest`="1" WHERE `theme`="RavenIce"';
-	$rc = @mysqli_query($conn, $sql);
+	$rc = mysqli_query($conn, $sql);
 	if (!$rc) die('Unable to update ' . $prefix . '_themes table to set defaults. MySQL reported: ' . mysqli_error($conn) . '<br />' . $sql);
 
 	echo '<script type="text/javascript">alert(\'Congratulations! Configuration has been completed. Please continue with the next step within the installation instructions.\');</script>';
