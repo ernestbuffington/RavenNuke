bc9e226737b0554cd0b79405eebc603e93fb4c3f
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-22 21:46:01 -0400

    Update wiw.inc.php

--- html/includes/RWS_WhoIsWhere/wiw.inc.php
+++ html/includes/RWS_WhoIsWhere/wiw.inc.php
@@ -1,19 +1,19 @@
 <?php
-/**********************************************************************/
-/* PHP-NUKE: Web Portal System								*/
-/* ===========================								*/
-/*													*/
-/* Copyright (c) 2002 by Francisco Burzi							*/
-/* http://phpnuke.org										*/
-/*													*/
-/* This program is free software. You can redistribute it and/or modify		*/
-/* it under the terms of the GNU General Public License as published by		*/
-/* the Free Software Foundation; either version 2 of the License.			*/
+/*************************************************************************/
+/* PHP-NUKE: Web Portal System								             */
+/* ===========================								             */
+/*													                     */
+/* Copyright (c) 2002 by Francisco Burzi							     */
+/* http://phpnuke.org										             */
+/*													                     */
+/* This program is free software. You can redistribute it and/or modify	 */
+/* it under the terms of the GNU General Public License as published by	 */
+/* the Free Software Foundation; either version 2 of the License.		 */
+/*************************************************************************/
 /*********************************************************************/
-/*********************************************************************/
-/* WhoIsWhere by Gaylen Fraley (aka Raven) 						*/
-/* http://www.ravenwebhosting.com								*/
-/* https://www.ravenphpscripts.com								*/
+/* WhoIsWhere by Gaylen Fraley (aka Raven) 						     */
+/* http://www.ravenwebhosting.com								     */
+/* https://www.ravenphpscripts.com								     */
 /*********************************************************************/
 
 global $db, $name, $prefix, $user, $userInfo, $user_prefix;
