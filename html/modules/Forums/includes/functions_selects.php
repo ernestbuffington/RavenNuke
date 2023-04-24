f57da76d30477f37af76bddb8e037c7359d10048
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-24 16:39:18 -0400

    Update functions_selects.php
    /* Applied rules: Ernest Allen Buffington (TheGhost) 04/24/2023 4:38 PM
     * WhileEachToForeachRector
     * EregToPregMatchRector (http://php.net/reference.pcre.pattern.posix https://stackoverflow.com/a/17033826/1348344 https://docstore.mik.ua/orelly/webprog/pcook/ch13_02.htm)
     * ListToArrayDestructRector (https://wiki.php.net/rfc/short_list_syntax https://www.php.net/manual/en/migration71.new-features.php#migration71.new-features.symmetric-array-destructuring)
     * NullToStrictStringFuncCallArgRector
     */

--- html/modules/Forums/includes/functions_selects.php
+++ html/modules/Forums/includes/functions_selects.php
@@ -100,10 +100,9 @@ function language_select($default, $select_name = "language", $dirname="modules/
         reset($lang);
 
         $lang_select = '<select name="' . $select_name . '">';
-        while ( list($displayname, $filename) = each($lang) )
-        {
-                $selected = ( strtolower((string) $default) == strtolower((string) $filename) ) ? ' selected="selected"' : '';
-                $lang_select .= '<option value="' . $filename . '"' . $selected . '>' . ucwords((string) $displayname) . '</option>';
+        foreach ($lang as $displayname => $filename) {
+            $selected = ( strtolower((string) $default) == strtolower((string) $filename) ) ? ' selected="selected"' : '';
+            $lang_select .= '<option value="' . $filename . '"' . $selected . '>' . ucwords((string) $displayname) . '</option>';
         }
         $lang_select .= '</select>';
 
@@ -150,10 +149,9 @@ function tz_select($default, $select_name = 'timezone')
         }
         $tz_select = '<select name="' . $select_name . '">';
 
-        while( list($offset, $zone) = each($lang['tz']) )
-        {
-                $selected = ( $offset == $default ) ? ' selected="selected"' : '';
-                $tz_select .= '<option value="' . $offset . '"' . $selected . '>' . $zone . '</option>';
+        foreach ($lang['tz'] as $offset => $zone) {
+            $selected = ( $offset == $default ) ? ' selected="selected"' : '';
+            $tz_select .= '<option value="' . $offset . '"' . $selected . '>' . $zone . '</option>';
         }
         $tz_select .= '</select>';
 
