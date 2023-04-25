2e12d1657af17db3f2472b94258bdc38264bec49
Ernest Buffington <ernest.buffington@gmail.com>
2023-04-24 21:17:55 -0400

    Update saveaddfield.php
    /* Applied rules: Ernest Allen Buffington (TheGhost) 04/24/2023 9:17 PM
     * CountOnNullRector (https://3v4l.org/Bndc9)
     * NullToStrictStringFuncCallArgRector
     */

--- html/modules/Your_Account/admin/saveaddfield.php
+++ html/modules/Your_Account/admin/saveaddfield.php
@@ -14,6 +14,12 @@
 /*  CNB Your Account http://www.phpnuke.org.br
 /*  NSN Your Account by Bob Marion, http://www.nukescripts.net
 /**************************************************************************/
+
+/* Applied rules: Ernest Allen Buffington (TheGhost) 04/24/2023 9:17 PM
+ * CountOnNullRector (https://3v4l.org/Bndc9)
+ * NullToStrictStringFuncCallArgRector
+ */
+ 
 if (!defined('YA_ADMIN'))
 {
    header('Location: ../../../index.php');
@@ -22,12 +28,12 @@ if (!defined('YA_ADMIN'))
 
 if (($radminsuper==1) OR ($radminuser==1)) {
 
-if (isset($field_name) && count($field_name) > 0) {
+if (isset($field_name) && (is_countable($field_name) ? count($field_name) : 0) > 0) {
   foreach ($field_name as $key => $var) {
     $field_size[$key] = intval($field_size[$key]);
     $field_pos[$key] = intval($field_pos[$key]);
-    $field_name[$key] = addslashes($field_name[$key]);
-    $field_value[$key] = addslashes($field_value[$key]);
+    $field_name[$key] = addslashes((string) $field_name[$key]);
+    $field_value[$key] = addslashes((string) $field_value[$key]);
     //$result = $db -> sql_query("SELECT '".$field_name[$key]."' FROM ".$user_prefix."_users");
     //$num = $db -> sql_numrows($result);
     $db->sql_query("UPDATE ".$user_prefix."_users_fields SET name='$field_name[$key]', value='$field_value[$key]',size='$field_size[$key]',need='$field_need[$key]',pos='$field_pos[$key]', public='$field_public[$key]' WHERE fid='$key'");
@@ -36,8 +42,8 @@ if (isset($field_name) && count($field_name) > 0) {
 if ($mfield_name != "") {
   $mfield_size = intval($mfield_size);
 	$mfield_pos = intval($mfield_pos);
-  $mfield_name = addslashes($mfield_name);
-	$mfield_value = addslashes($mfield_value);
+  $mfield_name = addslashes((string) $mfield_name);
+	$mfield_value = addslashes((string) $mfield_value);
   //$result = $db -> sql_query("SELECT '".$mfield_name."' FROM ".$user_prefix."_users");
   //$num = $db -> sql_numrows($result);
 	$db->sql_query("INSERT INTO ".$user_prefix."_users_fields (name, value, size, need, pos, public) VALUES ('$mfield_name','$mfield_value','$mfield_size','$mfield_need','$mfield_pos','$mfield_public')");
